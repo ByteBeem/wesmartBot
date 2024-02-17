bd85db55c24876ef5f79f72da8fa0458619b8472
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-02-17 15:53:46 +0000

    new

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const googleTTS = require('google-tts-api');
 const OpenAI = require('openai');
 const { createWorker } = require('tesseract.js');
 
+
 const worker = createWorker();
+
 (async () => {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
 })();
+
 
 const app = express();
 
